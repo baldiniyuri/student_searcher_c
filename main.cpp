f925fd2c1d224090c7c44e2dfdc5daa3e09f646e
Yuri Baldini <yuriabaldini@gmail.com>
2021-03-27 12:35:10 -0300

    Comments added

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
-#include <stdio.h>
 
-
+// declarando arrays e variaveis de acordo com o pedido no exercicio
 int counter_register=11;
 char name[50][20] = {"Yuri Baldini", "Pedro Silva", "Jão Carlos", "Priscila Pereira","Joana Dark", "Roberto Alves",
                      "Júlio César", "Juliana Martinz", "Veronica Lisboa", "Alan Silva"};
@@ -9,39 +8,37 @@ char email[50][30] = {"yuri@gmail.com", "pedro@hotmail.com", "jão@gmail.com", "
                       "roberto@gmail.com", "julio@yahoo.com", "juliana@ig.com", "veronica@hotmail.com", "alan@ig.com"};
 int ru[50] = {3114938, 123456, 654321, 147258, 963852, 111111, 222222, 333333, 444444, 555555};
 
-
-
-void student_register(){
+void student_register(){ // Registrar alunos
 
     printf("***************************\n");
     printf("* Registro de novos aluno *\n");
     printf("***************************\n");
     printf("Por favor digite seu nome: \n");
-    scanf("%s", &name[counter_register]);
+    scanf("%s", &name[counter_register]); // aramzenar o nome na posição de counter, que se inicia com valor 11
     printf("Por favor digite seu e-mail: \n");
-    scanf("%s", &email[counter_register]);
+    scanf("%s", &email[counter_register]); // aramzenar o email na posição de counter, que se inicia com valor 11
     printf("Por favor digite seu RU: \n");
-    scanf("%d", &ru[counter_register]);
+    scanf("%d", &ru[counter_register]); // aramzenar o ru na posição de counter, que se inicia com valor 11
     printf("\n\n");
-    counter_register++;
+    counter_register++; // aumentar o valor do contador
 }
 
-void student_searcher(){
+void student_searcher(){ // Função para buscar o aluno
     int aux = 0;
     bool found = false;
     printf("***************************\n");
     printf("*    Buscador de alunos.  *\n");
     printf("***************************\n");
     printf("Por favor digite o RU do aluno para realizar a pesquisa: \n");
-    scanf("%d", &aux);
+    scanf("%d", &aux); // auxiliar ira armazenar o valor digitado.
 
     for(int i = 0; i < 49; i++){
-        if(ru[i] == aux){
+        if(ru[i] == aux){ // comparando o valor digitado com valores no array na posição de i
             printf("Aluno: %s, email: %s", &name[i], &email[i]);
-            found = true;
+            found = true; // se foi encontrado o boleano se torna True e não cai no proximo if
         }
     }
-    if(!found){
+    if(!found){ // se não for encontrado printar
         printf("Aluno não encontrado.\n");
     }
     printf("\n\n");
@@ -49,12 +46,11 @@ void student_searcher(){
 
 
 int main(){
-        setlocale(LC_ALL, "");
+    setlocale(LC_ALL, "");
 
-        int option, command;
-
-        do
-        {
+    int option, command;
+    do
+        { // menu do programa
             printf("*****Yuri Baldini - RA 3114938******\n");
             printf("************************************\n");
             printf("*    Buscador de alunos            *\n");
@@ -72,7 +68,7 @@ int main(){
             while ((command = getchar()) != '\n' && command != EOF) {}   // Limpar buffer do terminal.
 
 
-            switch (option)
+            switch (option) // selecionar opções
 
             {
 
